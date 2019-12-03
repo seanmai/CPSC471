46cfc6875a1e82028fe25076096bcc225ea7aec4
Junhyeok <jhk8169@naver.com>
2019-12-02 23:39:48 -0700

    insert

--- seeddb.js
+++ seeddb.js
@@ -41,7 +41,7 @@ function seedDB() {
                 Rstrnt_id int NOT NULL, \
                 Table_no int NOT NULL, \
                 Capacity int NOT NULL, \
-                Res_id int NOT NULL, \
+                Res_id int, \
                 PRIMARY KEY (Rstrnt_id,Table_no), \
                 FOREIGN KEY (Rstrnt_id) REFERENCES RESTAURANT(Rstrnt_id), \
                 FOREIGN KEY (Res_id) REFERENCES RESERVATION(Res_id) )',
@@ -71,7 +71,6 @@ function seedDB() {
                 Employee_id int AUTO_INCREMENT NOT NULL, \
                 Name varchar(255) NOT NULL, \
                 Phone varchar(20) NOT NULL, \
-                Employer_id int NOT NULL, \
                 Rstrnt_id int NOT NULL, \
                 PRIMARY KEY (Employee_id), \
                 FOREIGN KEY (Employer_id) REFERENCES OWNER(Owner_id), \
@@ -146,10 +145,25 @@ function seedDB() {
     sql.forEach((statement) => {
         runSQL(statement);
     });
-
+    console.log("INSERTING ROWS")
     // Populating tables
     sql = [ 'INSERT INTO RESTAURANT (Name, Location, Avg_rate, Owner_id) VALUES ("Seans Pub", "1234 4th St Calgary, AB", 12, 1)',
             'INSERT INTO RESTAURANT (Name, Location, Avg_rate, Owner_id) VALUES ("Boba", "123 3rd St Calgary, AB", 15, 2)',
+            'INSERT INTO TABLES (Rstrnt_id, Table_no, Capacity, Res_id) VALUES (1, 1, 4, 1)',
+            'INSERT INTO CUSTOMER (Name, Phone) VALUES ("jh", "403-1111-1111")',
+            'INSERT INTO ADMIN (Name, Phone) VALUES ("James", "403-2222-2222")',
+            'INSERT INTO HELPS (Admin_id, Customer_id) VALUES (1,1)',
+            'INSERT INTO OWNER (Name, Phone) VALUES ("Smith","403-3333-3333")',
+            'INSERT INTO EMPLOYEE (Name, Phone, Rstrnt_id) VALUES ("Lauren", "403-4444-4444",1)',
+            'INSERT INTO FOOD (Name, Description, Price, Quantity, Rstrnt_id) VALUES ("Cheese Hamburger","Hamburger with cheese",10,2,1)',
+            'INSERT INTO FOOD_INGREDIENTS (Food_id, Food_ingredients) VALUES (1, "Cheese")',
+            'INSERT INTO DRINK (Name, Description, Quantity, Type, Rstrnt_id) VALUES ("Coke","cool",4,"soft drink",1)',
+            'INSERT INTO ORDERS (Pay_method, Date, Address, Pickup_time, Order_type, Customer_id, Rstrnt_id) VALUES ("visa","2019-12-01","Avenue 17th St","11:27:30","delivery",1,1)',
+            'INSERT INTO CONSIST_OF_FOOD (Order_id, Food_id) VALUES (1, 1)',
+            'INSERT INTO CONSIST_OF_DRINK (Order_id, Drink_id) VALUES (1, 1)',
+            'INSERT INTO MENU (Name,Meals_type,Rstrnt_id) VALUES ("Lunch set", "Lunch", 1)',
+            'INSERT INTO COMPRISES_OF_FOOD(Menu_id, Food_id) VALUES (1,1)',
+            'INSERT INTO COMPRISES_OF_DRINK(Menu_id, Drink_id) VALUES (1,1)',
           ];
         
     sql.forEach((statement) => {
